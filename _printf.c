a2442c828f7185c1cf7fd76f69e1435ce3280830
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-03-26 18:30:34 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,51 +1,49 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
-* _printf - produces output according to a format
-* @format: format specifier
-* @...:other variables
-* Return: int
-*/
+ * _printf - prints anything
+ * @format: the format string
+ *
+ * Return: number of bytes printed
+ */
 int _printf(const char *format, ...)
 {
-va_list args;
-va_start(args, format);
-int count = 0;
-char c;
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
-while ((c = *format++))
-{
-if (c == '%')
-{
-c = *format++;
-if (c == 'c')
-{
-putchar(va_arg(args, int));
-count++;
-}
-else if (c == 's')
-{
-char *s = va_arg(args, char *);
-while (*s)
-{
-putchar(*s++);
-count++;
-}
-}
-else if (c == '%')
-{
-putchar('%');
-count++;
-}
-}
-else
-{
-putchar(c);
-count++;
-}
-}
-va_end(args);
-return (count);
+	va_start(ap, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, ap);
+		if (*p != '%')
+		{
+			sum += _putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag char */
+		{
+			p++; /* next char */
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
+	}
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
 }
